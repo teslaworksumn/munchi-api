8f6acae3da664f9ccbf6f3aa9cbb74e8645ef4bb
Tukkur VanVark <vanv0123@umn.edu>
2017-03-30 15:29:58 -0500

    3/30

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
+var cors           = require('cors');
 var Mongo          = require('mongodb').MongoClient,
     assert = require('assert');
 var bodyParser     = require('body-parser');
@@ -15,6 +16,7 @@ var assert = require('assert');
 var dbConfig = require('./config/db');
 
 var port = process.env.PORT || 3000; // set our port
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,19 +32,22 @@ app.use(express.static(__dirname + '/public'));
 require('./app/routes')(app); // pass our application into our routes
 
 app.get('/api/recipes', function(req, res){
-    Mongo.connect(dbConfig.url, function(err, db){
-        var recipes = [];   
+    Mongo.connect(dbConfig.url, function(err, db){ 
         var col_cursor = db.collection('Recipes').find().toArray(function(err, docs){
             res.json(docs);
         });
         db.close();
     });
-   
 });
 
-app.post('/api/rpi/turnOn', function(req, res){
-   console.log(req.body);
-   res.send('success');
+//app.post('/rpi/select', function(req, res){
+//   console.log(req.body);
+//   res.send('success');
+//});
+
+app.post('http://localhost:5000/rpi/select', function(req,res){
+    console.log(req.body);
+    req.send();
 });
 
 // start api ===============================================
